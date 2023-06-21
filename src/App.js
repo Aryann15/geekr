cf461fd2ab1dae7e7b7870dc0eb9b0378e08f578
Aryann15 <rajlysm15@gmail.com>
2023-06-22 03:42:59 +0530

    routing of each page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-function App() {
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Onboarding from "./pages/Onboarding";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+const App = () => {
   return (
-    <h1>geekr</h1>
-  );
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/onboarding" element={<Onboarding />}/>
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App

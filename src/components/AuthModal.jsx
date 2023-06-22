6cdd1ede13a7609f261c799360fbfb5ded774780
Aryann15 <rajlysm15@gmail.com>
2023-06-23 01:39:01 +0530

    initial form now importing usestate for email and pasword

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,5 +1,5 @@
 const AuthModal = ({setShowModal}) => {
-
+import { useState } from "react"
 
     const handleClick= () => {
         setShowModal (false)
@@ -16,7 +16,15 @@ const AuthModal = ({setShowModal}) => {
         <div className="auth-button" onClick={handleClick}>X</div>
         <h2>{isSignUp ? 'Create Account' : 'Log in'}</h2>
         <p>By clicking Log in, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <form onSubmit={handleSubmit}></form>
+        <form onSubmit={handleSubmit}>
+            <input type="email" 
+            id="email"
+            name="email"
+            password="email"
+            required={true}
+            onChange={(e)=> setEmail(e.target.value)}
+            />
+        </form>
         AUTH MODAL
     </div>
   )

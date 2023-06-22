0a46530fcff126e555e97b84155f0a1437be6fe9
Aryann15 <rajlysm15@gmail.com>
2023-06-23 01:31:10 +0530

    logic for the form

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -4,9 +4,20 @@ const AuthModal = ({setShowModal}) => {
     const handleClick= () => {
         setShowModal (false)
     }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+    const isSignUp =true;
+
   return (
-    <div>
-        <button onClick={handleClick}>X</button>
+    <div className="auth-model">
+        <div className="auth-button" onClick={handleClick}>X</div>
+        <h2>{isSignUp ? 'Create Account' : 'Log in'}</h2>
+        <p>By clicking Log in, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <form onSubmit={handleSubmit}></form>
+        AUTH MODAL
     </div>
   )
 }

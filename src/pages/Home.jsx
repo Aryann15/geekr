61047a730bcb7970665cdfbf83242b2ddc17f7c6
Aryann15 <rajlysm15@gmail.com>
2023-06-22 23:50:16 +0530

    when we click the modal the modal value is set to true and it shows up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,32 @@
+import Nav from "../components/Nav";
+import { useState } from "react";
+import AuthModal from "../components/AuthModal";
+
 
 const Home = () => {
-    const authToken= true;
-  return (
-    <div className="home">
-        <h1>Let's Connect</h1>
-        <button className='primary-button'>
-            {authToken ? "Signout": "Create Account"}
-        </button>
-    </div>
-    
-  )
-}
+  const authToken = false;
 
-export default Home
+const [showModal,setShowModal] = useState(false)
+
+  const handleClick = () => {
+    console.log("clicked");
+    setShowModal(true)
+  };
+
+  return (
+    <>
+    <div className='overlay'>
+      <Nav authToken={authToken} />
+      <div className="home">
+        <h1>Let's Connect</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+
+        {showModal && <AuthModal setShowModal={setShowModal} />}
+      </div></div>
+    </>
+  );
+};
+
+export default Home;

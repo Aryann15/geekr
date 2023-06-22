7e181f2c45cb703133a0d7558cb341d30c171f07
Aryann15 <rajlysm15@gmail.com>
2023-06-23 01:49:14 +0530

    form

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,33 +1,66 @@
-const AuthModal = ({setShowModal}) => {
-import { useState } from "react"
+import { useState } from "react";
 
-    const handleClick= () => {
-        setShowModal (false)
-    }
+const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState(null);
+  const [password,setPassword]= useState(null);
+  const [confirmPassword,setConfirmPassword] = useState(null);
+  const[error,setError]= useState(null)
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-    }
 
-    const isSignUp =true;
+  console.log(email,password,confirmPassword )
+
+  const handleClick = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const isSignUp = true;
 
   return (
     <div className="auth-model">
-        <div className="auth-button" onClick={handleClick}>X</div>
-        <h2>{isSignUp ? 'Create Account' : 'Log in'}</h2>
-        <p>By clicking Log in, Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <form onSubmit={handleSubmit}>
-            <input type="email" 
-            id="email"
-            name="email"
-            password="email"
-            required={true}
-            onChange={(e)=> setEmail(e.target.value)}
-            />
-        </form>
-        AUTH MODAL
-    </div>
-  )
-}
+      <div className="auth-button" onClick={handleClick}>
+        X
+      </div>
+      <h2>{isSignUp ? "Create Account" : "Log in"}</h2>
+      <p>
+        By clicking Log in, Lorem ipsum dolor sit amet consectetur adipisicing
+        elit.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password-check"
+          id="password-check"
+          name="password-check"
+          placeholder="confirm password"
+          required={true}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input lassName="secondary-button" type="submit"/>
+        <p>{error}</p>
 
-export default AuthModal
+      </form>
+      AUTH MODAL
+    </div>
+  );
+};
+
+export default AuthModal;

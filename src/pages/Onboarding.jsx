e5dd955a45f43327af1227bd5d8b99eafd49129e
Aryann15 <rajlysm15@gmail.com>
2023-06-24 04:50:22 +0530

    input for the onboarind page

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from "react"
+import Nav from "../components/Nav"
+
 
 const Onboarding = () => {
+
+  const handleSubmit = () =>{
+  }
+  const handleChange = () => {}
   return (
-    <div>Onboarding</div>
+    <>
+    <Nav
+    setShowModal= {() => {}}
+    showModal ={false}
+    />
+    <div className="onboarding">
+      <h2>CREATE ACCOUNT</h2>
+      <form onSubmit={handleSubmit}>
+        <section>
+          <label htmlFor="first_name">First Name</label> 
+          <input 
+          id='first_name'
+          type="text"
+          name="first_name"
+          placeholder="First Name"
+          required={true} 
+          value={""}
+          onChange={handleChange}/>
+        </section>
+      </form>
+    </div>
+    </>
   )
 }
 

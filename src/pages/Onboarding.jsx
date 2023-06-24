d6c09dd3f02f9bdb7302a4003ca0575eb2b6dcd2
Aryann15 <rajlysm15@gmail.com>
2023-06-24 05:48:20 +0530

    input for roles

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -24,68 +24,102 @@ const Onboarding = () => {
 
             <label> BirthDate</label>
             <div className="multiple-input">
-            <input
-              id="dob_day"
-              type="number"
-              name="dob_day"
-              placeholder="DD"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
+              <input
+                id="dob_day"
+                type="number"
+                name="dob_day"
+                placeholder="DD"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
 
-            <input
-              id="dob_month"
-              type="number"
-              name="dob_month"
-              placeholder="MM"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
-            <input
-              id="dob_year"
-              type="number"
-              name="dob_year"
-              placeholder="YYYY"
-              required={true}
-              value={""}
-              onChange={handleChange}
-            />
+              <input
+                id="dob_month"
+                type="number"
+                name="dob_month"
+                placeholder="MM"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+              <input
+                id="dob_year"
+                type="number"
+                name="dob_year"
+                placeholder="YYYY"
+                required={true}
+                value={""}
+                onChange={handleChange}
+              />
+            </div>
+            <br></br><br></br>
+            <label>Gender</label>
+            <div className="multiple-input">
+              <input
+                id="man_gender_id"
+                type="radio"
+                name="gender_identity"
+                value="man"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="man_gender_id">Male</label>
+              <input
+                id="woman_gender_id"
+                type="radio"
+                name="gender_identity"
+                value="woman"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="woman_gender_id">Female</label>
+
+              <input
+                id="more_gender_id"
+                type="radio"
+                name="gender_identity"
+                value="more"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="more_gender_id">More</label>
+
+<br></br><br></br>
+              <label>Who are you?</label>
+              <div className="multiple-input">
+              <input
+                id="front_end_developer"
+                type="radio"
+                name="front-end"
+                value="front_end_developer"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="front-end_dev">Front End Developer</label>
+              
+              <input
+                id="backend_developer"
+                type="radio"
+                name="backend"
+                value="backend_developer"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="backend_developer">Backend Developer</label>
+              
+              <input
+                id="Fullstack_developer"
+                type="radio"
+                name="fullstack_developer"
+                value="fullstack-dev"
+                onChange={handleChange}
+                checked={false}
+              />
+              <label htmlFor="Fullstack">Fullstack Developer</label>
+
 </div>
-
-
-<label >Gender</label> 
-<div className="multiple-input">
-
-          <input 
-          id='man_gender_id'
-          type="radio"
-          name="gender_identity"
-          
-          value='man'
-          onChange={handleChange}
-          checked = {false} />
-<label htmlFor="man_gender_id">Male</label>
-<input 
-          id='woman_gender_id'
-          type="radio"
-          name="gender_identity"
-          value='woman'
-          onChange={handleChange}
-          checked = {false} />
-          <label htmlFor="woman_gender_id">Female</label>
-
-<input 
-          id='more_gender_id'
-          type="radio"
-          name="gender_identity"
-          
-          value='more'
-          onChange={handleChange}
-          checked = {false} />
-          <label htmlFor="more_gender_id">More</label>
-          </div>
+            </div>
           </section>
         </form>
       </div>
